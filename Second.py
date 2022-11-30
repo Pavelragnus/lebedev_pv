f4714ae276cacba37e214483c09ea26ee1127265
Pavelragnus <mr.nabludatel@mail.ru>
2022-11-30 20:42:37 +0300

    Create an expression table

--- Second.py
+++ Second.py
@@ -14,9 +14,9 @@ air_quality = pd.concat([air_quality_pm25, air_quality_no2], axis=0) - слож�
 
 wf = df[['geo_accession', 'platform_id', 'source_name_ch1']] #wf - working frame
 macrophage= wf[wf['source_name_ch1'].str.contains('macrophage|macrophages', case=False)]
-macrophage.insert(2, "Cell type", 'macrophage')
+macrophage.insert(3, "Cell type", 'macrophage')
 conditions = [
-    (macrophage['source_name_ch1'].str.contains('[B,m]one[-,\s]marrow', case=False, regex=True)),
+    (macrophage['source_name_ch1'].str.contains('[Bm]one(-|\s)marrow', case=False, regex=True)),
     (macrophage['source_name_ch1'].str.contains('Peritoneal', case=False)),
     (macrophage['source_name_ch1'].str.contains('Alveolar|lung', case=False)),
     (macrophage['source_name_ch1'].str.contains('tumor|tumour', case=False)),
@@ -27,39 +27,39 @@ conditions = [
     (macrophage['source_name_ch1'].str.contains('Peripheral', case=False)),
     (macrophage['source_name_ch1'].str.contains('muscle', case=False)),
     (macrophage['source_name_ch1'].str.contains('1 day after TBI', case=False)),
-    (~macrophage['source_name_ch1'].str.contains('[B,m]one[-,\s]marrow|Peritoneal|Alveolar|tumor|tumour|Splenic|lung|hepatic|sciatic nerve|Phagocytic|Peripheral|muscle|1 day after TBI' , case=False))
+    (~macrophage['source_name_ch1'].str.contains('[Bm]one(-|\s)marrow|Peritoneal|Alveolar|tumor|tumour|Splenic|lung|hepatic|sciatic nerve|Phagocytic|Peripheral|muscle|1 day after TBI' , case=False))
 ]
 
 values = ['Bone Marrow Derived macrophages', 'Peritoneal macrophage', 'Alveolar macrophage', 'tumor macrophage','Splenic macrophages','hepatic macrophages', 'sciatic nerve macrophages','Phagocytic macrophages', 'Peripheral macrophages','muscle macrophages','1 day after TBI macrophages', 'other macrophage']
-macrophage.insert(3, "Classes", np.select(conditions, values))
+macrophage.insert(4, "Classes", np.select(conditions, values))
 macrophage.to_csv('macrophage.csv')
 
 '''
 create a table of T-cells
 '''
-wf = df[['geo_accession', 'platform_id', 'source_name_ch1']] #wf - working frame
-T_cells= wf[wf['source_name_ch1'].str.contains('[/s]T[-,\s]cell[\s,s]|[\s]CD8[+,\s][\s]| [\s]CD4[+,\s][\s]', case=False)]
-T_cells.insert(2, "Cell type", 'T_cells')
+
+T_cells= wf[wf['source_name_ch1'].str.contains('(/s)+T(-|\s)cell(\s|s)|(\s)+CD8(+|\s)(\s)+| (\s)CD4(+|\s)(\s)+', case=False)]
+T_cells.insert(3, "Cell type", 'T_cells')
 conditions = [
-    (T_cells['source_name_ch1'].str.contains('[\s]CD8[+,\s][\s]',case=False, regex=True)),
-    (T_cells['source_name_ch1'].str.contains('[\s]CD4[+,\s][\s]',case=False, regex=True)),
+    (T_cells['source_name_ch1'].str.contains('(\s)+CD8(\+|\s)(\s)+',case=False, regex=True)),
+    (T_cells['source_name_ch1'].str.contains('(\s)+CD4(+|\s)(\s)+',case=False, regex=True)),
     (T_cells['source_name_ch1'].str.contains('YFP+', case=False, regex=True)),
-    (T_cells['source_name_ch1'].str.contains('V\xce\xb34', case=False)),
+    (T_cells['source_name_ch1'].str.contains('V\\xce\\xb34', case=False)),
     (T_cells['source_name_ch1'].str.contains('Tumor-infiltrating', case=False)),
     (T_cells['source_name_ch1'].str.contains('Thymus|Thymic', case=False)),
     (T_cells['source_name_ch1'].str.contains('Splenic|spleen', case=False)),
-    (~T_cells['source_name_ch1'].str.contains('[\s]CD8[+,\s]|[\s]CD4[+,\s][\s]|YFP+|V\xce\xb34|Tumor-infiltrating|Thymus|Thymic|Splenic|spleen', case=False, regex=True))
+    (~T_cells['source_name_ch1'].str.contains('(\s)+CD8(\+|\s)(\s)+|(\s)+CD4(+|\s)(\s)+|YFP+|V\\xce\\xb34|Tumor-infiltrating|Thymus|Thymic|Splenic|spleen', case=False, regex=True))
 ]
 values = ['CD8+ T cells', 'CD4+ T cells', 'YFP+ T cells','V\xce\xb34 T cell', 'Tumor-infiltrating T cells', 'Thymus T cells', 'Splenic T cells', 'other T cells']
-T_cells.insert(3, "Classes", np.select(conditions, values))
+T_cells.insert(4, "Classes", np.select(conditions, values))
 T_cells.to_csv('T_cells.csv')
 
 '''
 create a table of B-cells
 '''
-wf = df[['geo_accession', 'platform_id', 'source_name_ch1']] #wf - working frame
-B_cells= wf[wf['source_name_ch1'].str.contains('[\s]B[-, \s]cell[s,\s]', case=False, regex=True)]
-B_cells.insert(2, "Cell type", 'B_cells')
+
+B_cells= wf[wf['source_name_ch1'].str.contains('(\s)+B(-|\s)cell(s|\s)', case=False, regex=True)]
+B_cells.insert(3, "Cell type", 'B_cells')
 conditions = [
     (B_cells['source_name_ch1'].str.contains('splenic|from spleen|spleenic',case=False, regex=True)),
     (B_cells['source_name_ch1'].str.contains('Germinal center',case=False, regex=True)),
@@ -75,7 +75,7 @@ conditions = [
 
 ]
 values = ['splenic B cells', 'Germinal center B cells', 'Marginal zone B cells', 'Follicular B cells', 'B220+ B cells','Primary B cells','NP+ GC B cells','Immature B cells','CH12 line','CD19+ B cells', 'other B-cells']
-B_cells.insert(3, "Classes", np.select(conditions, values))
+B_cells.insert(4, "Classes", np.select(conditions, values))
 B_cells.to_csv('B_cells.csv')
 
 '''
@@ -85,4 +85,5 @@ B_cells=pd.read_csv('B_cells.csv')
 T_cells=pd.read_csv('T_cells.csv')
 macrophage=pd.read_csv('macrophage.csv')
 great_table = pd.concat([B_cells, T_cells, macrophage], axis=0)
+great_table = great_table.drop(great_table.columns[[0]], axis = 1)
 great_table.to_csv('great_table.csv')
